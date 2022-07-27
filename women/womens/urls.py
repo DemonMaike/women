38fe62213a844f5369234040dedaff8ad273683c
DemonMaike <dmitry.malyshevv@ya.ru>
2022-07-27 15:25:56 +0300

    Create templates and fill database

--- women/womens/urls.py
+++ women/womens/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import new
+from .views import *
 
 urlpatterns = [
-    path('', new),
+    path('', womens, name= 'womens'),
 ]
--- women/womens/views.py
+++ women/womens/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
-def new(request):
-    return HttpResponse('What are you doing?')
+def womens(request):
+    womens = Women.objects.all()
+    context = {
+        'womens': womens,
+    }
+    return render(request, 'index.html', context)
