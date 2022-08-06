57663216de0bfcfedc6bbb8f0de2d7b41c629237
DemonMaike <dmitry.malyshevv@ya.ru>
2022-08-06 15:18:30 +0300

    Create a toolbar and upgrade category functional

--- women/womens/urls.py
+++ women/womens/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', womens, name= 'womens'),
-]
+    path('', womens, name = 'womens'),
+    path('about/', about, name = 'about'),
+    path('cathigories/', cathigories, name = 'cathigories'),
+    path('contacts/', contacts, name = 'contacts'),
+    path('post/<int:post_id>/', post, name = 'post'),
+    path('login', login, name = 'login'),
+    path('category/<int:cat_id>/', show_category, name = 'category'),
+    ]
